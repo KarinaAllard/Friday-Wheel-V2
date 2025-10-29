cd5fcecfd9351de7258e9f5c27224e1e3d17c88e
Karina Allard <karina.allard.92@gmail.com>
2025-10-29 12:50:57 +0100

    chore: prepare for deployment on vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,22 @@ import entriesRouter from "./routes/entries"
 import connectDB from "./db";
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost:5173/",
+    "https://karinaallard.github.io/"
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}));
+
 app.use(express.json());
 
 connectDB();
