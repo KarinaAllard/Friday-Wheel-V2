7b1429207744fdecbf413e38a196b1ed8b50ca50
Karina Allard <karina.allard.92@gmail.com>
2025-10-29 13:11:17 +0100

    fix: change name and homepage to prep for github pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Friday-Wheel/',
+  base: '/Friday-Wheel-V2/',
   plugins: [react(), tailwindcss()],
 })
